9519820e3901322f4ea8a27abee57a0fc30b293b
Paulo Victor Campelo Selano <pselano@br.ibm.com>
2018-07-17 20:41:16 -0300

    ignore

--- server/db_connection/connect_db.py
+++ server/db_connection/connect_db.py
@@ -1,10 +1,24 @@
-# db connection
+# database connection file
 
+# imports
 from ibmdbpy import IdaDataBase, IdaDataFrame
 import json
-credentials_file = open('db_credentials.json').read()
-cred_json = json.loads(credentials_file)
-idaDb = IdaDataBase(dsn=cred_json['dsn'])
+import sys
+sys.path.append('../')
+
+# approach for getting env variables
+from settings import concatCredentials
+dsn_concatenate = concatCredentials()
+
+# db connection via json
+# credentials_file = open('db_credentials.json').read()
+# cred_json = json.loads(credentials_file)
+# idaDb = IdaDataBase(dsn=cred_json['dsn'])
+
+#must impost from .env
+
+# idaDb = IdaDataBase(dsn=dsn_concatenate)
+idaDb = IdaDataBase('DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIIP', uid='wjn20801', pwd='581j76tl49k17f^s')
 
 idaDb.show_tables()
 
