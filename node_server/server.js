8d9d4bb4a31276ff8f4470a1a4a1334b5dafdf37
PV Campelo Selano <pv_sf@hotmail.com>
2018-07-18 23:35:54 -0300

    refactored architecture and server render html

--- node_server/server.js
+++ node_server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  
-});
+app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
