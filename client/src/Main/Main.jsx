409c6bfb361c73fc868be49c445e01aea8c9661e
Paulo Victor Campelo Selano <pselano@br.ibm.com>
2018-07-27 10:20:47 -0300

    removed switch

--- client/src/Main/Main.jsx
+++ client/src/Main/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Switch } from 'react-router';
 import Header from '../Header/Header.jsx';
 import Home from '../Home/Home.jsx';
 import Playlists from '../Playlists/Playlists.jsx';
