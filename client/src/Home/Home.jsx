9a4870ff2da4a073f6623214e0090d3462e97fc8
Paulo Victor Campelo Selano <pselano@br.ibm.com>
2018-07-27 15:42:45 -0300

    fixed to relative font-sizes

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        Home 
+      <div className="home">
+        <div className="home__video">
+          <iframe className="home__video-frame" src="https://www.youtube.com/watch?v=b16fys0awu8&index=17&list=PLLzyoMBk3rplw4cVm6ZgxbDE6HIL-pVEL" frameborder="0" allowfullscreen/>
+        </div> 
       </div>
     );
   }
