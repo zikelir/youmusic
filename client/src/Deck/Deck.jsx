a2a4c979f95e664d84bd0e8e0de723a132a5f7db
PV Campelo Selano <pv_sf@hotmail.com>
2018-07-31 22:13:31 -0300

    started implementing player api

--- client/src/Deck/Deck.jsx
+++ client/src/Deck/Deck.jsx
@@ -4,7 +4,7 @@ class Deck extends React.Component {
   render() {
     return (
       <div className="deck">
-        <div className="deck__play-button"></div>
+        <div className="deck__button--play"></div>
       </div>
     );
   }
--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Player from '../Player/Player.jsx';
 import Deck from '../Deck/Deck.jsx';
 
 class Home extends React.Component {
@@ -6,7 +7,7 @@ class Home extends React.Component {
     return (
       <div className="home">
         <div className="home__video">
-          <iframe className="home__video-frame" src="https://www.youtube.com/watch?v=b16fys0awu8&index=17&list=PLLzyoMBk3rplw4cVm6ZgxbDE6HIL-pVEL" frameBorder="0"/>
+          <Player /> {/* <iframe className="home__video-frame" src="https://www.youtube.com/watch?v=b16fys0awu8&index=17&list=PLLzyoMBk3rplw4cVm6ZgxbDE6HIL-pVEL" frameBorder="0"/> */}
         </div>
         <Deck/>
       </div>
